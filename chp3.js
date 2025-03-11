cbb3dbff9c3767aea15b565d2c7742012ef416ef
bhagyeshbhavsar1 <bhagyeshbhavsar07@gamil.com>
2025-03-11 23:04:22 +0530

    chp 3 Q 1 done

---
+++ chp3.js
@@ -0,0 +1,9 @@
+//minimum output fuction Define the function min that takes two arguments and returns their minimum.
+
+
+const min = function(a,b){
+
+    return Math.min(a,b);
+}
+console.log(min(3,4));
+console.log(min(13,8));
