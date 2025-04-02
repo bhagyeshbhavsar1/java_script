429d69cbce3634560022cdd09d176788dd5af982
bhagyeshbhavsar1 <bhagyeshbhavsar07@gamil.com>
2025-04-02 18:27:45 +0530

    arrays

--- array.js
+++ array.js
@@ -1,6 +1,6 @@
 let animes = [ "naruto", "one-piece" , "bleach" , "dragon-ball" , "horimiya" ];
 
-/*animes[1] = "naruto shippuden";
+animes[1] = "naruto shippuden";
 
 
 animes.push( "solo leveling");
@@ -12,14 +12,14 @@ animes.unshift("hunter x hunter");
 animes.shift();
 
 let index = animes.indexOf("naruto");
-let indexs = animes.indexOf("baka");*/
+let indexs = animes.indexOf("baka");
 
 //array sorting 
 
-//animes.sort();
+animes.sort();
 animes.sort().reverse();
 
 console.log(animes);
-/*console.log("the size of the array is" +list_anime);
+console.log("the size of the array is" +list_anime);
 console.log(index);
-console.log(indexs);*/
+console.log(indexs);
