7c663d3393fbecc0dc7d910b6b74bbe99241d1a9
bhagyeshbhavsar1 <bhagyeshbhavsar07@gamil.com>
2025-04-02 18:20:09 +0530

    arrays

---
+++ array.js
@@ -0,0 +1,25 @@
+let animes = [ "naruto", "one-piece" , "bleach" , "dragon-ball" , "horimiya" ];
+
+/*animes[1] = "naruto shippuden";
+
+
+animes.push( "solo leveling");
+animes.pop();
+
+let list_anime = animes.length;
+
+animes.unshift("hunter x hunter");
+animes.shift();
+
+let index = animes.indexOf("naruto");
+let indexs = animes.indexOf("baka");*/
+
+//array sorting 
+
+//animes.sort();
+animes.sort().reverse();
+
+console.log(animes);
+/*console.log("the size of the array is" +list_anime);
+console.log(index);
+console.log(indexs);*/
